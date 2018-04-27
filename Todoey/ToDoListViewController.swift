3a9a7f175a3e3a5378f1dbfb82b7c8237d435480
Hoda Moustapha <hodamoustapha@hodas-macbook-pro.local>
2018-04-27 14:01:53 -0400

    Complete interface stage1

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ToDoListViewController.swift
+//  Todoey
+//
+//  Created by Hoda Moustapha on 4/27/18.
+//  Copyright © 2018 Hoda Moustapha. All rights reserved.
+//
+
+import UIKit
+
+// * * * * * * * * * * * * * * * * * * * * * * * * begin class
+class ToDoListViewController: UITableViewController
+{
+    // % % % % % % % % % % % % % % % %
+    let itemArray = ["Get Milk", "Complete IOS Todoey", "Complete Perspective", "Wash Dishes" ];
+    
+    // + - + - + - + - + - + - + - + - + - + - + - + - + - + - + -
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // + - + - + - + - + - + - + - + - + - + - + - + - + - + - + -
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK - tableView DataSource Methods
+    // + - + - + - + - + - + - + - + - + - + - + - + - + - + - + -
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row];
+        return cell;
+    }
+    
+   // + - + - + - + - + - + - + - + - + - + - + - + - + - + - + -
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return itemArray.count;
+    }
+  
+    //MARK - tableView Delegate Methods
+     // + - + - + - + - + - + - + - + - + - + - + - + - + - + - + -
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print(itemArray[indexPath.row]);
+        
+    
+       
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark)
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none;
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark;
+        }
+        
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true);
+        
+        
+    }
+}// * * * * * * * * * * * *  end class
+
