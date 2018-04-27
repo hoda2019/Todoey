08f68ca89e7edca1304079ce94db7294890c9971
Hoda Moustapha <hodamoustapha@hodas-macbook-pro.local>
2018-04-27 18:07:54 -0400

    complete add function stage1

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class ToDoListViewController: UITableViewController
 {
     // % % % % % % % % % % % % % % % %
-    let itemArray = ["Get Milk", "Complete IOS Todoey", "Complete Perspective", "Wash Dishes" ];
+    var itemArray = ["Get Milk", "Complete IOS Todoey", "Complete Perspective", "Wash Dishes" ];
     
     // + - + - + - + - + - + - + - + - + - + - + - + - + - + - + -
     override func viewDidLoad()
@@ -60,11 +60,35 @@ class ToDoListViewController: UITableViewController
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark;
         }
         
-        
-        
         tableView.deselectRow(at: indexPath, animated: true);
+    }
+    
+    
+ //MARK - add new item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem)
+    {
+        var textField = UITextField();
         
+        let alert = UIAlertController(title: "Add new Item", message: "", preferredStyle: .alert);
+        
+        let action = UIAlertAction(title: "Add Item", style: .default)
+        { (action) in
+            self.itemArray.append(textField.text!);
+            self.tableView.reloadData();
+        }
+        
+        alert.addTextField
+        {
+            (alertTextField) in
+            alertTextField.placeholder = "Create new Item";
+            textField = alertTextField;
+        }
+        
+        alert.addAction(action);
+        present (alert, animated: true, completion: nil)
         
     }
+    
 }// * * * * * * * * * * * *  end class
 
